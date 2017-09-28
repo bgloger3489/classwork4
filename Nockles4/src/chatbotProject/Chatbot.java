a186351d39f58ab576b1951e4db9f9609649ff48
Teacher <Teacher@10.8.39.80>
2017-09-28 11:46:16 -0400

    Setup for Chatbot project

---
+++ Nockles4/src/chatbotProject/Chatbot.java
@@ -0,0 +1,31 @@
+package chatbotProject;
+
+public class Chatbot {
+
+	private String userName;
+	private Topic ben;
+	private boolean chatting;
+	
+	
+	public Chatbot() {
+		ben = new ChatbotBen();
+		userName = "unknown user";
+		chatting = true;
+	}
+
+	public void startTalking() {
+		ChatbotMain.print("Welcome to our chatbot! What is your name?");
+		userName = ChatbotMain.getInput();
+		while(chatting) {
+			ChatbotMain.print("What do you want to talk about?");
+			String response = ChatbotMain.getInput();
+			if(ben.isTriggered(response)) {
+				chatting = false;
+				ben.startChatting();
+			}else {
+				ChatbotMain.print("I'm sorry. I don't understand.");
+			}
+		}
+	}
+
+}
---
+++ Nockles4/src/chatbotProject/ChatbotBen.java
@@ -0,0 +1,9 @@
+package chatbotProject;
+
+public class ChatbotBen {
+
+	public ChatbotBen() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
