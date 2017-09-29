0d442292b079b33bdf9548e57a4eaea3102d528b
Teacher <Teacher@10.8.39.80>
2017-09-29 11:12:01 -0400

    Finished setup

--- Nockles4/src/chatbotProject/Chatbot.java
+++ Nockles4/src/chatbotProject/Chatbot.java
@@ -16,12 +16,13 @@ public class Chatbot {
 	public void startTalking() {
 		ChatbotMain.print("Welcome to our chatbot! What is your name?");
 		userName = ChatbotMain.getInput();
+		chatting = true;
 		while(chatting) {
 			ChatbotMain.print("What do you want to talk about?");
 			String response = ChatbotMain.getInput();
 			if(ben.isTriggered(response)) {
 				chatting = false;
-				ben.startChatting();
+				ben.startChatting(response);
 			}else {
 				ChatbotMain.print("I'm sorry. I don't understand.");
 			}
