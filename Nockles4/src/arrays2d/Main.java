093eae4d57a87c61163574c9919454a57125b7be
Teacher <Teacher@10.8.39.80>
2017-10-20 11:48:40 -0400

    Introduced AIIOOBE

---
+++ Nockles4/src/arrays2d/Main.java
@@ -0,0 +1,31 @@
+package arrays2d;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * increases the element in arr at index psn
+	 * and decreases the elements at psn -1 and psn + 1,
+	 * if they exist. PRECONDITION: arr[psn] exists
+	 * 
+	 * THIS idea is another major topic in AP CSA.
+	 * It is the idea of avoiding an ArrayIndexOutOfBoundsException
+	 * you will see it on quizzes, tests, and project rubrics
+	 * SO ALWAYS BE MINDFUL!
+	 * @param arr
+	 * @param psn
+	 */
+	public static void changeNeighbors(int[] arr, int psn) {
+		
+	}
+
+	
+	
+	
+	
+	
+}
